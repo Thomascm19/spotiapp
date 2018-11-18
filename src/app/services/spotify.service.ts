91b8b22115d9cb8ef4789fe639491e8c2a9d9812
Thomas <thomascaycedo19@outlook.com>
2018-11-17 22:10:01 -0500

    Se optimiza el codigo P2

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,24 +13,26 @@ export class SpotifyService {
 
    }
 
-   getNewReleases(){
-const headers = new HttpHeaders({
+   getQuery(query: string){
+     const url = `https://api.spotify.com/v1/${query}`;
 
-  'Authorization':'Bearer BQDHTgB46FdyYnWBCAMqsudfZmHx41IDOj26aa8-XJtXKgNTrSkcaieYqUzVtEvEzRRO0l9WmR0nKjxx6_E'
-});
-
-      return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{headers})
-            .pipe(map( data =>  data['albums'].items
-            ))
-   }
-
-   getArtista(termino:string){
-    const headers = new HttpHeaders({
+     const headers = new HttpHeaders({
 
       'Authorization':'Bearer BQDHTgB46FdyYnWBCAMqsudfZmHx41IDOj26aa8-XJtXKgNTrSkcaieYqUzVtEvEzRRO0l9WmR0nKjxx6_E'
     });
-    
-          return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,{headers})
+
+    return this.http.get(url, {headers});
+   }
+
+   getNewReleases(){
+
+      return this.getQuery('browse/new-releases?limit=20') 
+      .pipe(map( data =>  data['albums'].items));
+        }
+
+   getArtista(termino:string){
+
+          return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
           .pipe(map( data => data['artists'].items));
 
    }
